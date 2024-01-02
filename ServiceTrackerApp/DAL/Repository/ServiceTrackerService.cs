8038769496483e046703b1673a579570b8fe20fe
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-02 12:49:31 +0530

    Add files via upload

---
+++ ServiceTrackerApp/DAL/Repository/ServiceTrackerService.cs
@@ -0,0 +1,53 @@
+﻿using ServiceTrackerApp.DAL.Interface;
+using ServiceTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ServiceTrackerApp.DAL.Repository
+{
+    public class ServiceTrackerService : IServiceTrackerInterface
+    {
+        private IServiceTrackerRepository _repo;
+        public ServiceTrackerService(IServiceTrackerRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        public int DeleteService(int ServiceId)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Service GetServiceByID(int ServiceId)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public void Save()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+
+        IEnumerable<Service> IServiceTrackerInterface.GetServices()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        Service IServiceTrackerInterface.InsertService(Service Service)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        bool IServiceTrackerInterface.UpdateService(Service Service)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
