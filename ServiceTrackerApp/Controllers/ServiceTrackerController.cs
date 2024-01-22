b7d0d38f5eaa7082d00f48d15bc5b9323c7f4c67
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-22 19:03:40 +0530

    Add files via upload

---
+++ ServiceTrackerApp/Controllers/ServiceTrackerController.cs
@@ -0,0 +1,70 @@
+﻿using ServiceTrackerApp.DAL.Interface;
+using ServiceTrackerApp.DAL.Repository;
+using ServiceTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiceTrackerApp.Controllers
+{
+    public class ServiceTrackerController : Controller
+    {
+        private readonly IServiceTrackerInterface _Repository;
+        public ServiceTrackerController(IServiceTrackerInterface service)
+        {
+            _Repository = service;
+        }
+        public ServiceTrackerController()
+        {
+            // Constructor logic, if needed
+        }
+        // GET: ServiceTracker
+        public ActionResult Index()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public ViewResult Details(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Create()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Service Service)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult EditAsync(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        public ActionResult Edit(Service Service)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ServiceTrackerApp/DAL/Repository/ServiceTrackerRepository.cs
@@ -0,0 +1,61 @@
+﻿using ServiceTrackerApp.DAL.Interface;
+using ServiceTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ServiceTrackerApp.DAL.Repository
+{
+    public class ServiceTrackerRepository : IServiceTrackerRepository
+    {
+        private ServiceTrackerDbContext _context;
+        public ServiceTrackerRepository(ServiceTrackerDbContext Context)
+        {
+            this._context = Context;
+        }
+        public IEnumerable<Service> GetServices()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public Service GetServiceByID(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public Service InsertService(Service Service)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public int DeleteService(int ServiceID)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public bool UpdateService(Service Service)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public void Save()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
